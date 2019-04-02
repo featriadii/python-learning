efedf02d3d6752962eefcb0f475839e1c3b01404
Aditya Fitriadi <aditya.fitriadi99@gmail.com>
2019-04-02 12:59:51 +0700

    penjumlahan

---
+++ coba.py
@@ -0,0 +1,12 @@
+import hitung
+
+pesan = "ini kode program utama"
+print(pesan)
+
+x = float( input("Masukkan nilai ke-1 = "))
+y = float( input("Masukkan nilai ke-2 = "))
+
+print("Ini fungsi %s" %(hitung.__str__))
+
+hasil = hitung.tambah(x,y)
+print("Hasil penjumlahan %f dan %f adalah %f" %(x,y,hasil))
